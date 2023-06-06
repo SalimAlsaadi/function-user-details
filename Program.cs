40e3a7bd3e12f0fafee166d32bf58c220a57adf8
SalimAlsaadi <al-saadis-s-s@hotmail.com>
2023-06-05 23:55:13 -0700

    function for user details

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            static void getUsersDetails()
+            {
+                Console.WriteLine("Enter your name");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter lastName");
+                string lastName = Console.ReadLine();
+
+                Console.WriteLine("Enter your Age");
+                string age = Console.ReadLine();
+                int integerAge = int.Parse(age);
+
+                Console.WriteLine("name : " + name + " " + lastName +  " , age: " + integerAge);
+
+            }
+
+            getUsersDetails();
+            getUsersDetails();
+
+        }
+    }
+}
